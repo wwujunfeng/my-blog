9907f7ab467cceb31e2995e6a69332c800ab18e5
wjf <736668996@qq.com>
2026-03-07 17:38:52 +0800

    修改name/test push

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -4,19 +4,19 @@
       <div class="hero-content">
         <div class="hero-text">
           <h1 class="hero-title">
-            你好，我是 <span class="highlight">吴俊锋</span>
+            你好，我是 <span class="highlight">白日梦想家</span>
           </h1>
           <p class="hero-description">
-            一名热爱前端开发的工程师，专注于创造美观且功能强大的网站
+            一名充满创意的3D设计师，专注于创造震撼的视觉效果和沉浸式体验
           </p>
           <div class="hero-buttons">
-            <a href="#portfolio" class="btn btn-primary">查看作品</a>
+            <a href="#portfolio" class="btn btn-primary">查看设计</a>
             <a href="#contact" class="btn btn-secondary">联系我</a>
           </div>
           <div class="hero-social">
             <a href="#" class="social-link"><i class="fa fa-github"></i></a>
-            <a href="#" class="social-link"><i class="fa fa-linkedin"></i></a>
-            <a href="#" class="social-link"><i class="fa fa-twitter"></i></a>
+            <a href="#" class="social-link"><i class="fa fa-artstation"></i></a>
+            <a href="#" class="social-link"><i class="fa fa-behance"></i></a>
             <a href="#" class="social-link"><i class="fa fa-instagram"></i></a>
           </div>
         </div>
